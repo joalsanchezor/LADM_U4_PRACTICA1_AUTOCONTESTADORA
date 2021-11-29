87b596cd9207f58921a304f3766a3f780e997866
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-11-29 00:24:11 -0700

    Frontend.

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_autocontestadora/MainActivity2.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u4_practica1_autocontestadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
