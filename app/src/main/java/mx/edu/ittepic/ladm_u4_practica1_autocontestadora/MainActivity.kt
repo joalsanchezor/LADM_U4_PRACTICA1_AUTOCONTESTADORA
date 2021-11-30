880f5717fc9e1388e5af0d048bdd7b5dd82c4c9d
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-11-29 20:06:42 -0700

    Pantalla principal.

--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_autocontestadora/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_autocontestadora/MainActivity.kt
@@ -5,14 +5,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    val db = Firebase.firestore
+    val db = FirebaseFirestore.getInstance()
+    var listaLlamadasPerdidas = ArrayList<String>()
+    var desactivadaContestadora = true
+    var activadaAutocontestadora = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +38,44 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, Array(1) { Manifest.permission.READ_CALL_LOG }, 101)
         }
 
+        cargarLlamadasPerdidas()
         button.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
+        }
+
+        button2.setOnClickListener {
+            if(desactivadaContestadora == true){
+                desactivadaContestadora = false
+                activadaAutocontestadora = true
+                button2.setText("AUTOCONTESTADORA ACTIVADA")
+                Toast.makeText(this, "SE HA ACTIVADO LA AUTOCONTESTADORA", Toast.LENGTH_SHORT).show()
+            }else{
+                desactivadaContestadora = true
+                activadaAutocontestadora = false
+                button2.setText("ACTIVAR AUTOCONTESTADORA")
+                Toast.makeText(this, "SE HA DESACTIVADO LA AUTOCONTESTADORA", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun cargarLlamadasPerdidas() {
+        listaLlamadasPerdidas.clear()
+        db.collection("LISTALLAMADAS").addSnapshotListener { value, error ->
+            if(error != null){
+                return@addSnapshotListener
+            }
+            value!!.documents
+            for (i in value){
+                val llamada = "NÚMERO: ${i.getString("numero")}\nTIPO DE CONTACTO: ${i.getString("contactoDeseado")}"
+                listaLlamadasPerdidas.add(llamada.toString())
+            }
+            if (listaLlamadasPerdidas.isEmpty()){
+                val respuesta = "SIN LLAMADAS PERDIDAS"
+                listaLlamadasPerdidas.add(respuesta)
+            }
+
+            listViewLlamadasPerdidas.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaLlamadasPerdidas)
         }
     }
 
--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_autocontestadora/PhoneStateReceiver.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_autocontestadora/PhoneStateReceiver.kt
@@ -20,55 +20,60 @@ class PhoneStateReceiver : BroadcastReceiver() {
     var listaNegra = ArrayList<String>()
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if(intent!!.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_RINGING){
-            telephonyState = true
-        }
+        val obj = MainActivity()
+        if(obj.activadaAutocontestadora == true){
+            if(intent!!.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_RINGING){
+                telephonyState = true
+            }
 
-        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_OFFHOOK){
-            telephonyState = false
-        }
+            if(intent.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_OFFHOOK){
+                telephonyState = false
+            }
 
-        if(telephonyState && intent.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_IDLE){
-            val numero = intent.extras?.getString("incoming_number")
-            db.collection("CONTACTOS").addSnapshotListener { value, error ->
-                if(error !=null)
-                {
-                    return@addSnapshotListener
-                }
-                listaNegra.clear()
-                listaBlanca.clear()
-
-                for (i in value!!){
-                    val numero = i.get("telefono")
-                    if(i.getString("contactoDeseado")!!.equals("true")){
-                        listaBlanca.add(numero.toString())
-                    }else{
-                        listaNegra.add(numero.toString())
+            if(telephonyState && intent.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_IDLE){
+                val numero = intent.extras?.getString("incoming_number")
+                db.collection("CONTACTOS").addSnapshotListener { value, error ->
+                    if(error !=null)
+                    {
+                        return@addSnapshotListener
                     }
-                }
+                    listaNegra.clear()
+                    listaBlanca.clear()
+
+                    for (i in value!!){
+                        val num = i.get("telefono")
+                        if(i.getString("contactoDeseado")!!.equals("true")){
+                            listaBlanca.add(num.toString())
+                        }else{
+                            listaNegra.add(num.toString())
+                        }
+                    }
 
 
-                if(listaBlanca.contains(numero.toString())){
-                    val SMS = SmsManager.getDefault()
-                    SMS.sendTextMessage(numero,null,"DISCULPA, TE LLAMO EN UN RATO.",null,null)
-                    //Toast.makeText(context, "ENTRANDO: ${numero}",Toast.LENGTH_LONG).show()
-                    registrarLlamada(context!!, numero.toString(), "DISCULPA, TE LLAMO EN UN RATO.")
-                }
+                    if(listaBlanca.contains(numero.toString())){
+                        val SMS = SmsManager.getDefault()
+                        Toast.makeText(context, "ENVIANDO MENSAJE...",Toast.LENGTH_LONG).show()
+                        SMS.sendTextMessage(numero,null,"DISCULPA, TE LLAMO EN UN RATO.",null,null)
+                        Toast.makeText(context, "¡MENSAJE ENVIADO!",Toast.LENGTH_LONG).show()
+                        registrarLlamada(context!!, numero.toString(), "DISCULPA, TE LLAMO EN UN RATO.","CONTACTO DESEADO")
+                    }
 
-                if(listaNegra.contains(numero.toString())){
-                    val SMS = SmsManager.getDefault()
-                    SMS.sendTextMessage(numero,null,"NO DEVOLVERE TU LLAMADA, POR FAVOR NO INSISTAS",null,null)
-                    //Toast.makeText(context, "ENTRANDO: ${numero}",Toast.LENGTH_LONG).show()
-                    registrarLlamada(context!!, numero.toString(), "NO DEVOLVERE TU LLAMADA, POR FAVOR NO INSISTAS")
+                    if(listaNegra.contains(numero.toString())){
+                        val SMS = SmsManager.getDefault()
+                        SMS.sendTextMessage(numero,null,"NO DEVOLVERE TU LLAMADA, POR FAVOR NO INSISTAS",null,null)
+                        //Toast.makeText(context, "ENTRANDO: ${numero}",Toast.LENGTH_LONG).show()
+                        registrarLlamada(context!!, numero.toString(), "NO DEVOLVERE TU LLAMADA, POR FAVOR NO INSISTAS","CONTACTO NO DESEADO")
+                    }
                 }
             }
         }
     }
 
-    private fun registrarLlamada(cont: Context, numero: String, mensaje: String){
+    private fun registrarLlamada(cont: Context, numero: String, mensaje: String, contactoDeseado: String){
         var llamada: MutableMap<String, String> = HashMap()
         llamada.put("numero",numero)
         llamada.put("mensaje",mensaje)
+        llamada.put("contactoDeseado", contactoDeseado)
 
         db.collection("LISTALLAMADAS").document()
             .set(llamada)
